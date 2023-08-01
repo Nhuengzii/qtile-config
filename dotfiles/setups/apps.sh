0c060ec51a2a6bd08af2781169f044a4edb0d4ae
Nhuengzii <32nhueng@gmail.com>
2023-08-02 00:38:14 +0700

    add npm to `apps` script

--- dotfiles/setups/apps.sh
+++ dotfiles/setups/apps.sh
@@ -1,2 +1,3 @@
 yay -Sy discord neovim vim lazygit microsoft-edge-dev-bin github-cli kitty \
-  docker zsh noto-fonts-emoji nodejs cargo go xclip "visual-studio-code-bin" --noconfirm
+  docker zsh noto-fonts-emoji nodejs cargo go xclip "visual-studio-code-bin" \
+  npm --noconfirm
