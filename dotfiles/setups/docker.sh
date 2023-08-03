f50c5428e3b36fbe891eb7238d341d87ecda123f
Nhuengzii <32nhueng@gmail.com>
2023-08-04 00:34:33 +0700

    also install docker compose,buildx when setup docker

--- dotfiles/setups/docker.sh
+++ dotfiles/setups/docker.sh
@@ -1,10 +1,10 @@
 # check is docker is installed
 if [ -x "$(command -v docker)" ]; then
     echo "Docker is already installed"
+    sudo pacman -Syu docker-compose docker-buildx --noconfirm
 else
     echo "Installing Docker"
-    sudo pacman -S docker --noconfirm
+    sudo pacman -S docker docker-compose docker-buildx --noconfirm
 fi
-
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
