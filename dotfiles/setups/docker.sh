568a283095b7c20277fc062cd2f02a2f5ee3a2bc
Nhuengzii <32nhueng@gmail.com>
2023-08-04 00:14:12 +0700

    install docker is not already install

--- dotfiles/setups/docker.sh
+++ dotfiles/setups/docker.sh
@@ -1,2 +1,10 @@
+# check is docker is installed
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker is already installed"
+else
+    echo "Installing Docker"
+    sudo pacman -S docker --noconfirm
+fi
+
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
