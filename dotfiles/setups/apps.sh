5ee9bb3f7693beaef887030cec72798ac5272471
Nhuengzii <32nhueng@gmail.com>
2023-08-06 10:15:50 +0700

    add youtube-music to app install script

--- dotfiles/setups/apps.sh
+++ dotfiles/setups/apps.sh
@@ -1,3 +1,3 @@
 yay -Sy discord neovim vim lazygit microsoft-edge-dev-bin github-cli kitty \
   docker zsh noto-fonts-emoji nodejs cargo go xclip "visual-studio-code-bin" \
-  npm xfce4-screenshooter --noconfirm
+  npm xfce4-screenshooter youtube-music-bin --noconfirm
