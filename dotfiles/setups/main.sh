f338bd3ab90f896fcf6e00b52e8cfc5acd700ad8
Nhuengzii <32nhueng@gmail.com>
2023-08-21 10:13:22 +0700

    add main script in dotfiles setups

---
+++ dotfiles/setups/main.sh
@@ -0,0 +1,7 @@
+sh ~/.config/qtile/dotfiles/setups/git.sh
+sh ~/.config/qtile/dotfiles/setups/zsh.sh
+sh ~/.config/qtile/dotfiles/setups/apps.sh
+sh ~/.config/qtile/dotfiles/setups/fonts.sh
+sh ~/.config/qtile/dotfiles/setups/docker.sh
+sh ~/.config/qtile/dotfiles/setups/symlink.sh
+sh ~/.config/qtile/dotfiles/setups/default_browser.sh
