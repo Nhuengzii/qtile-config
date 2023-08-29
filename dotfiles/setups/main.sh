d66d4e34f5f0c9b612c1013b36f10959e7178e77
Nhuengzii <32nhueng@gmail.com>
2023-08-29 13:47:56 +0700

    update main setup script

--- dotfiles/setups/main.sh
+++ dotfiles/setups/main.sh
@@ -1,7 +1,8 @@
 sh ~/.config/qtile/dotfiles/setups/git.sh
-sh ~/.config/qtile/dotfiles/setups/zsh.sh
 sh ~/.config/qtile/dotfiles/setups/apps.sh
 sh ~/.config/qtile/dotfiles/setups/fonts.sh
 sh ~/.config/qtile/dotfiles/setups/docker.sh
 sh ~/.config/qtile/dotfiles/setups/symlink.sh
 sh ~/.config/qtile/dotfiles/setups/default_browser.sh
+sh ~/.config/qtile/dotfiles/setups/zsh.sh
+Sh ~/.config/qtile/dotfiles/setups/neovim/astronvim.sh
